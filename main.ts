fded83dbd5d791824d34dbf8b94540298060bdb0
youss-uiux <youssif.aboubacar@outlook.fr>
2024-12-17 10:57:24 +0100

    implement addition class for basic calculator functionality

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-console.log("First calculator")
+console.log("First calculator");
+
+class Adittion {
+	private a: number;
+    private b: number;
+    addition: number;
+
+	constructor(n: number, m: number) {
+		this.a = n;
+        this.b = m;
+        this.addition = this.a + this.b;
+	}
+}
+
+const calc = new Adittion(2, 3);
+console.log(calc.addition);
