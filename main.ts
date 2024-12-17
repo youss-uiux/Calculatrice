662de3e1dd2462d804b2a45b29fa62d0120b9483
youss-uiux <youssif.aboubacar@outlook.fr>
2024-12-17 10:58:15 +0100

    add Substraction class for basic arithmetic operations

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
 console.log("First calculator")
+
+class Substraction {
+    private a: number;
+    private b: number;
+
+    constructor(a: number, b: number) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public substract(): number {
+        return this.a - this.b;
+    }
+}
