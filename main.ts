3a5552d4f413d274b53df41b89f7c4c8c5e3f9b7
youss-uiux <youssif.aboubacar@outlook.fr>
2025-01-15 12:09:18 +0100

    add Subtraction class for basic arithmetic operations

--- main.ts
+++ main.ts
@@ -12,5 +12,17 @@ class Adittion {
 	}
 }
 
+class Subtraction {
+    private a: number;
+    private b: number;
+    subtraction: number;
+
+    constructor(n: number, m: number) {
+        this.a = n;
+        this.b = m;
+        this.subtraction = this.a - this.b;
+    }
+}
+
 const calc = new Adittion(2, 3);
 console.log(calc.addition);
